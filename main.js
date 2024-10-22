7917ab69fcfee62a987415088a1721ad75d374a1
hiteshkumar85 <hiteshkumar853391@gmail.com>
2024-10-22 21:30:54 +0530

    new sign in page

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 function move_login(){
     document.querySelector('.sign_in').classList.remove('move');
     document.querySelector('.log_in').classList.add('move');
+    console.log('hk');
 }
 function move_sign(){
     document.querySelector('.sign_in').classList.add('move');
     document.querySelector('.log_in').classList.remove('move');
+    console.log('hkk');
 }
 
 
