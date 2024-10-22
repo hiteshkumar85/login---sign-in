e0e63c44c323c4aa686d10273eed974f70c490b8
hiteshkumar85 <hiteshkumar853391@gmail.com>
2024-10-22 20:45:20 +0530

    h

---
+++ main.js
@@ -0,0 +1,10 @@
+function move_login(){
+    document.querySelector('.sign_in').classList.remove('move');
+    document.querySelector('.log_in').classList.add('move');
+}
+function move_sign(){
+    document.querySelector('.sign_in').classList.add('move');
+    document.querySelector('.log_in').classList.remove('move');
+}
+
+
